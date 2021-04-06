03c645b4c237cfdc54f800aca59bcb98e647eb9b
Ian Davies <mr.daviesian@gmail.com>
2021-04-06 15:59:20 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // type task into input field
+  let form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", function(e){
+    e.preventDefault()
+
+    let newTaskDescription = e.target[0].value
+
+    let li = document.createElement('li')
+    li.innerText = `${newTaskDescription}`
+
+    let ul = document.getElementById('tasks')
+    ul.append(li)
+
+    //<button onclick="myFunction()">Try it</button>
+    let button = document.createElement('button')
+    button.innerHTML = "Remove Task"
+    button.addEventListener('click', function(e){
+      ul.removeChild(ul.childNodes)
+    })
+    li.append(button)
+  })
+
+  
 });
